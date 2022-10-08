d177c7cf7009a1e27282038275a9b43bbecbb354
Tomáš Valášek <tomas.valasek@atreo.cz>
2022-10-08 09:28:37 +0200

    feat: .Net6 project since I work on Mac

--- Battleships.Test/GameTest.cs
+++ Battleships.Test/GameTest.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Battleships;
-using FluentAssertions;
-using Xunit;
+namespace Battleships.Test;
 
-namespace Battleships.Test
+public class GameTest
 {
-    public class GameTest
+    [Fact]
+    public void TestPlay()
     {
-        [Fact]
-        public void TestPlay()
-        {
-            var ships = new[] { "3:2,3:5" };
-            var guesses = new[] { "7:0", "3:3" };
-            Game.Play(ships, guesses).Should().Be(0);
-        }
+        var ships = new[] { "3:2,3:5" };
+        var guesses = new[] { "7:0", "3:3" };
+        Game.Play(ships, guesses).Should().Be(0);
     }
 }
---
+++ Battleships/Program.cs
@@ -0,0 +1,9 @@
+namespace Battleships;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
